f6058d434295e2f4bc60f0041ecaeb30a61a1e54
aaachris1820 <63766879+aaachris1820@users.noreply.github.com>
2023-10-04 14:20:15 +0600

    using a rsc to render fetched data

--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -5,9 +5,9 @@ const Navbar = () => {
     return (
         <header className="w-10/12 mx-auto mt-6 pb-2 border-b-2">
             <nav className="flex justify-between ">
-                <Link href={'/'} className="text-3xl font-semibold">Home</Link>
+                <Link href={'/'} className="text-3xl font-bold">Home</Link>
 
-                <div className="flex gap-6 font-semibold">
+                <div className="flex gap-6 font-bold">
                     <NavLink routeLink={'/link1'} routeName={'Link1'}></NavLink>
                     <NavLink routeLink={'/link2'} routeName={'Link2'}></NavLink>
                     <NavLink routeLink={'/link3'} routeName={'Link3'}></NavLink>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
+interface User {
+  id: number,
+  name: string,
+  email: string
+}
+export default async function Home() {
+  
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const result: User[] = await res.json();
 
-export default function Home() {
   return (
-    <p>
-    this is home page
-    </p>
+    <main>
+      <section className="w-11/12 mx-auto my-10">
+        <p>this is home page</p>
+        <div className="grid grid-cols-3 my-6 gap-4">
+          {result.map((data) => <div className="p-4 border-2 rounded-lg" key={data.id}>
+            {data.email}
+          </div>)}
+        </div>
+      </section>
+    </main>
   )
 }
